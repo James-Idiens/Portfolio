eb8e8bb4c6c58d5d6c44b4d9bddb7cd19c8194c3
James Idiens <jidiens@outlook.com>
2023-07-18 11:44:56 +1200

    change website name

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
+  site_name: 'James Idiens',
+  title: 'James Idiens',
   description: 'Boilerplate built with Astro using React and Tailwind CSS',
   author: 'Emma',
   locale_region: 'en-us',
   locale: 'en',
-};
+}
